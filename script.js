d8bdfc5789a275c0f0c94840d11ef9477cbf9f77
GianluigiCelentano <cgianluigi96@gmail.com>
2021-07-20 10:48:49 +0200

    wip js

--- script.js
+++ script.js
@@ -7,42 +7,42 @@ const app = new Vue ({
         {
           name: "bed",
           price: 25,
-          img: "bed.jpg"
+          img: "img/bed.jpg"
         },
         {
           name: "bone",
           price: 10,
-          img: "bone.jpg"
+          img: "img/bone.jpg"
         },
         {
           name: "treats",
           price: 10,
-          img: "treats.jpg"
+          img: "img/treats.jpg"
         },
         {
           name: "dryfood",
           price: 10,
-          img: "dry food.jpg"
+          img: "img/dry food.jpg"
         },
         {
           name: "chewabletoy",
           price: 10,
-          img: "chewable toy.jpg"
+          img: "img/chewable toy.jpg"
         },
         {
           name: "chewableweight",
           price: 15,
-          img: "chewable weight.jpg"
+          img: "img/chewable weight.jpg"
         },
         {
           name: "trasportbag",
           price: 25,
-          img: "trasport bag.jpg"
+          img: "img/trasport bag.jpg"
         },
         {
           name: "catleash",
           price: 10,
-          img: "cat leash.jpg"
+          img: "img/cat leash.jpg"
         },
       ]
     }, 
